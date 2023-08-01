4b0dc31e8953c54dcc5ba1390a2e3eb0a4e2cf23
Gabriel Teles <gabz.teles@gmail.com>
2023-08-01 13:11:33 -0300

    feat: minor visual improvement

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -26,8 +26,8 @@ const backgroundStyle = css`
   right: 0;
   bottom: 0;
   width: 100%;
-  background: rgba(255, 255, 255, 0.8);
-  backdrop-filter: blur(2px);
+  background: rgba(255, 255, 255, 0.7);
+  backdrop-filter: blur(10px);
 `;
 
 const contentWrapperStyle = css`
